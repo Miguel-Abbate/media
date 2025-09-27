13a280e309ac2045b67bd987b1faae0b42bf2fe5
Miguel-Abbate <abbatejr@gmail.com>
2025-09-26 22:46:32 -0300

    Primeira versão

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿
+Console.WriteLine("Digite o 1° numero");
+string um = Console.ReadLine()!;
+Console.WriteLine("Digite o 2° numero");
+string dois = Console.ReadLine()!;
+Console.WriteLine("Digite o 3° numero");
+string tres = Console.ReadLine()!;
+
