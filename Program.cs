edaa1c15d1f8f64d2981181e9e496f31d0a2c6c7
Miguel-Abbate <abbatejr@gmail.com>
2025-09-27 10:45:39 -0300

    2° versão

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
-﻿Console.Clear();
+﻿using System.Drawing;
+using System.Runtime.InteropServices;
+
+Console.Clear();
 Console.WriteLine("Digite o 1° numero");
 string um = Console.ReadLine()!;
 double one = Convert.ToDouble(um);
@@ -10,6 +13,9 @@ string tres = Console.ReadLine()!;
 double three = Convert.ToDouble(tres);
 double soma = one + two + three;
 double media = 3;
-Console.WriteLine($" Média= { soma /media:N2}");
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine($" Média= {soma / media:N2}");
+Console.ResetColor();
+
 
 
