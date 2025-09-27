d6ad5e9a61e4d53e8a6de799ca4f0fedb92a8f3c
Miguel-Abbate <abbatejr@gmail.com>
2025-09-27 10:37:51 -0300

    2° versão

--- Program.cs
+++ Program.cs
@@ -1,8 +1,15 @@
-﻿
+﻿Console.Clear();
 Console.WriteLine("Digite o 1° numero");
 string um = Console.ReadLine()!;
+double one = Convert.ToDouble(um);
 Console.WriteLine("Digite o 2° numero");
 string dois = Console.ReadLine()!;
+double two = Convert.ToDouble(dois);
 Console.WriteLine("Digite o 3° numero");
 string tres = Console.ReadLine()!;
+double three = Convert.ToDouble(tres);
+double soma = one + two + three;
+double media = 3;
+Console.WriteLine($" Média= { soma /media:N2}");
+
 
